31b5f54a6c8e8514eddf241e07845a9f82a978b5
Kaunda da Gama <dannydagamaisutic@gmail.com>
2025-02-10 15:21:35 +0100

    Inicio do Projecto

---
+++ src/main/java/com/hotel/diamante/DiamanteApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.diamante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiamanteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hotel/diamante/entity/Booking.java
@@ -0,0 +1,70 @@
+package com.hotel.diamante.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "bookings")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = " data de entrada não pode nula")
+    private LocalDate checkeIn;
+    @NotBlank(message = "data de saída não pode nula")
+    @Future(message = " data de saída não pode ser nopassado")
+    private LocalDate checkOut;
+    @Min(value = 1, message = "O numero de adultos não pode ser 0")
+    private int numberOfAdults;
+    @Min(value = 0, message = "O numero de crianças não pode ser menos que 0")
+    private int numberOfChildren;
+    private int totalNumberGuest;
+    private String bookingConfirmationCOde;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name ="user_id")
+    private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="room_id")
+    private Room room;
+
+    public void setTotalNumberGuest() {
+        this.totalNumberGuest = numberOfAdults + numberOfChildren;
+    }
+
+    public void setNumberOfAdults(int numberOfAdults) {
+        this.numberOfAdults = numberOfAdults;
+        setTotalNumberGuest();
+    }
+
+    public void setNumberOfChildren(int numberOfChildren) {
+        this.numberOfChildren = numberOfChildren;
+        setTotalNumberGuest();
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "id=" + id +
+                ", checkeIn=" + checkeIn +
+                ", checkOut=" + checkOut +
+                ", numberOfAdults=" + numberOfAdults +
+                ", numberOfChildren=" + numberOfChildren +
+                ", totalNumberGuest=" + totalNumberGuest +
+                ", bookingConfirmationCOde='" + bookingConfirmationCOde + '\'' +
+                ", user=" + user +
+                ", room=" + room +
+                '}';
+    }
+}
