f51cefdfabbd7c73e6e6f062f15afc810a9e6240
Kaunda da Gama <dannydagamaisutic@gmail.com>
2025-02-10 16:10:03 +0100

    Criação de Dtos

---
+++ src/main/java/com/hotel/diamante/dto/BookingDTO.java
@@ -0,0 +1,36 @@
+package com.hotel.diamante.dto;
+
+import com.hotel.diamante.entity.Room;
+import com.hotel.diamante.entity.User;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookingDTO {
+    private Long id;
+    @NotBlank(message = " data de entrada não pode nula")
+    private LocalDate checkeIn;
+    @NotBlank(message = "data de saída não pode nula")
+    @Future(message = " data de saída não pode ser nopassado")
+    private LocalDate checkOut;
+    @Min(value = 1, message = "O numero de adultos não pode ser 0")
+    private int numberOfAdults;
+    @Min(value = 0, message = "O numero de crianças não pode ser menos que 0")
+    private int numberOfChildren;
+    private int totalNumberGuest;
+    private String bookingConfirmationCOde;
+
+    private UserDTO user;
+    private RoomDTO room;
+}
---
+++ src/main/java/com/hotel/diamante/entity/Room.java
@@ -0,0 +1,42 @@
+package com.hotel.diamante.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "rooms")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int roomNumber;
+    private  String roomType;
+    private BigDecimal price;
+    private String Photo;
+    private String description;
+
+    private List<Booking>bookingList= new ArrayList<Booking>();
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", roomNumber=" + roomNumber +
+                ", roomType='" + roomType + '\'' +
+                ", price=" + price +
+                ", Photo='" + Photo + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
