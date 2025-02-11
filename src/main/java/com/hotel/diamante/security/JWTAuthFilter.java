6d1e1483a3937e57966704d06e85ea3af479ec43
Kaunda da Gama <dannydagamaisutic@gmail.com>
2025-02-11 10:31:43 +0100

    Implementação de autenticação

--- src/main/java/com/hotel/diamante/security/JWTAuthFilter.java
+++ src/main/java/com/hotel/diamante/security/JWTAuthFilter.java
@@ -2,13 +2,22 @@ package com.hotel.diamante.security;
 
 import com.hotel.diamante.utils.JWTUtils;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.CachingUserDetailsService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Component
-public class JWTAuthFilter {
+public class JWTAuthFilter extends OncePerRequestFilter {
     private final JWTUtils jwtUtils;
     private final CachingUserDetailsService cachingUserDetailsService;
 
@@ -16,7 +25,31 @@ public class JWTAuthFilter {
         this.jwtUtils = jwtUtils;
         this.cachingUserDetailsService = cachingUserDetailsService;
     }
-    protected  void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain){
+    protected  void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)throws ServletException, IOException {
+        final String authHeader= request.getHeader("Authorization");
+        final String jwtToken;
+        final String userEmail;
+        if(authHeader == null || authHeader.isBlank()) {
+            filterChain.doFilter(request, response);
+            return;
+
+        }
+        jwtToken=authHeader.substring(7);
+        userEmail=jwtUtils.extractUsername(jwtToken);
+        if(userEmail!=null && SecurityContextHolder.getContext().getAuthentication()==null) {
+            UserDetails userDetails = cachingUserDetailsService.loadUserByUsername(userEmail);
+            if(jwtUtils.isValidateToken(jwtToken, userDetails)) {
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                securityContext.setAuthentication(authenticationToken);
+                SecurityContextHolder.setContext(securityContext);
+            }
+        }
+        filterChain.doFilter(request, response);
+
+
+
 
     }
 }
--- src/main/java/com/hotel/diamante/service/CustomerUserDetailsService.java
+++ src/main/java/com/hotel/diamante/service/CustomerUserDetailsService.java
@@ -3,14 +3,15 @@ package com.hotel.diamante.service;
 import com.hotel.diamante.exception.CustomerException;
 import com.hotel.diamante.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomerUserDatilsService {
+public class CustomerUserDetailsService  implements UserDetailsService {
     private final UserRepository userRepository;
 
-    public CustomerUserDatilsService(UserRepository userRepository) {
+    public CustomerUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
